af57be09930e2583b7cb51a232150cc7b91de68a
Promise <promiseprince01@gmail.com>
2023-03-17 06:28:09 +0100

    express session start

--- routes/loginApp.js
+++ routes/loginApp.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 
 router.get("/login", (req, res) => {
-  res.send("You are logged in");
+  res.status(200).send("You are logged in");
   console.log("you are logged in")
 });
 
