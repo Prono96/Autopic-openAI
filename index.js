1ff83deba15750c52b42427fa316783c634e47eb
Promise <promiseprince01@gmail.com>
2023-03-19 09:45:34 +0100

    connected project to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const path = require('path') 
 const express = require('express');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://chiboy:${process.env.MONGODB_PASSWORD}@cluster0.xfvp9cv.mongodb.net/?retryWrites=true&w=majority`
+
+// Connection to the MongoDB
+mongoose.connect(uri).then(
+  (res) => { 
+    console.log("Connected to the data base")
+  },
+  err => { console.log(err) }
+);
 
 const app = express();
 
--- routes/loginApp.js
+++ routes/loginApp.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const router = express.Router()
+const session = require('express-session');
+const router = express.Router();
+const app = express();
+
+// Initialise the express-session
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 router.get("/login", (req, res) => {
+  console.log(res.session);
   res.status(200).send("You are logged in");
-  console.log("you are logged in")
+  console.log("you are logged in");
 });
 
 module.exports = router;
